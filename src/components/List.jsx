f31c3429b98e456d3bcb7c324522fdc5fe6129ab
tanmayagrwl <tanmayhp78@gmail.com>
2023-09-09 14:33:12 +0530

    🐛fix minor bug

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -15,84 +15,84 @@ function List(props) {
           key={item.Pid}
           className="flex w-auto h-auto items-center justify-center"
         >
-          <Link href={`/listing/${item.Pid}`}>
-            <div className="p-10 w-auto sm:w-auto lg:w-[15vw] rounded-xl bg-[#bbd8ad] flex flex-col items-center justify-center ">
+          <div className="p-10 w-auto sm:w-auto lg:w-[15vw] rounded-xl bg-[#bbd8ad] flex flex-col items-center justify-center ">
+            <Link href={`/listing/${item.Pid}`}>
               <Image
                 src={`/assets/plant${item.Pid}.png`}
                 alt="logo"
                 height={100}
                 width={100}
-              />
+              />{" "}
               <div className="w-full flex justify-center pt-10 font-bold font-sans text-xl text-[#002140]">
                 {item.name}
               </div>
               <div className="w-full flex justify-center pt-2 font-semibold font-sans text-md text-[#002140]">
                 $ {item.price}.00
               </div>
-              <div className=" flex justify-center pt-2 font-semibold font-sans text-md text-[#ffffff] items-center gap-4 bg-[#228f22] rounded-full pb-2 mt-4 w-32">
-                <button
-                  className="bg-[#228f22] rounded-full px-2 hover:bg-[#1b791b] active:bg-[#166916]"
-                  onClick={() => {
-                    if (item.Pid == 1 && state.count1 > 0) {
-                      state.count1 -= 1
-                      state.sum -= 1
-                    } else if (item.Pid == 2 && state.count2 > 0) {
-                      state.count2 -= 1
-                      state.sum -= 1
-                    } else if (item.Pid == 3 && state.count3 > 0) {
-                      state.count3 -= 1
-                      state.sum -= 1
-                    } else if (item.Pid == 4 && state.count4 > 0) {
-                      state.count4 -= 1
-                      state.sum -= 1
-                    } else if (item.Pid == 5 && state.count5 > 0) {
-                      state.count5 -= 1
-                      state.sum -= 1
-                    }
-                  }}
-                >
-                  -
-                </button>
-                <p>
-                  {item.Pid === 1
-                    ? state.count1
-                    : item.Pid === 2
-                    ? state.count2
-                    : item.Pid === 3
-                    ? state.count3
-                    : item.Pid === 4
-                    ? state.count4
-                    : item.Pid === 5
-                    ? state.count5
-                    : null}
-                </p>
+            </Link>
+            <div className=" flex justify-center pt-2 font-semibold font-sans text-md text-[#ffffff] items-center gap-4 bg-[#228f22] rounded-full pb-2 mt-4 w-32">
+              <button
+                className="bg-[#228f22] rounded-full px-2 hover:bg-[#1b791b] active:bg-[#166916]"
+                onClick={() => {
+                  if (item.Pid == 1 && state.count1 > 0) {
+                    state.count1 -= 1
+                    state.sum -= 1
+                  } else if (item.Pid == 2 && state.count2 > 0) {
+                    state.count2 -= 1
+                    state.sum -= 1
+                  } else if (item.Pid == 3 && state.count3 > 0) {
+                    state.count3 -= 1
+                    state.sum -= 1
+                  } else if (item.Pid == 4 && state.count4 > 0) {
+                    state.count4 -= 1
+                    state.sum -= 1
+                  } else if (item.Pid == 5 && state.count5 > 0) {
+                    state.count5 -= 1
+                    state.sum -= 1
+                  }
+                }}
+              >
+                -
+              </button>
+              <p>
+                {item.Pid === 1
+                  ? state.count1
+                  : item.Pid === 2
+                  ? state.count2
+                  : item.Pid === 3
+                  ? state.count3
+                  : item.Pid === 4
+                  ? state.count4
+                  : item.Pid === 5
+                  ? state.count5
+                  : null}
+              </p>
 
-                <button
-                  className="bg-[#228f22] hover:bg-[#1b791b] active:bg-[#166916] rounded-full px-2"
-                  onClick={() => {
-                    if (item.Pid == 1) {
-                      state.count1 += 1
-                      state.sum += 1
-                    } else if (item.Pid == 2) {
-                      state.count2 += 1
-                      state.sum += 1
-                    } else if (item.Pid == 3) {
-                      state.count3 += 1
-                      state.sum += 1
-                    } else if (item.Pid == 4) {
-                      state.count4 += 1
-                      state.sum += 1
-                    } else if (item.Pid == 5) {
-                      state.count5 += 1
-                      state.sum += 1
-                    }
-                  }}
-                >
-                  +
-                </button>
-              </div>
+              <button
+                className="bg-[#228f22] hover:bg-[#1b791b] active:bg-[#166916] rounded-full px-2"
+                onClick={() => {
+                  if (item.Pid == 1) {
+                    state.count1 += 1
+                    state.sum += 1
+                  } else if (item.Pid == 2) {
+                    state.count2 += 1
+                    state.sum += 1
+                  } else if (item.Pid == 3) {
+                    state.count3 += 1
+                    state.sum += 1
+                  } else if (item.Pid == 4) {
+                    state.count4 += 1
+                    state.sum += 1
+                  } else if (item.Pid == 5) {
+                    state.count5 += 1
+                    state.sum += 1
+                  }
+                }}
+              >
+                +
+              </button>
             </div>
-          </Link>
+          </div>
         </div>
       ))}
     </div>
